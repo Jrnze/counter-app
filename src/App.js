f2b1f93ff827a0fa1080febc16709ac3ad90f666
JOSE_LORENZO_ANDICOy <andicoylorenzo@gmail.com>
2022-11-17 10:48:22 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [value, setValue] = useState(0);
+  const addValue = () => setValue(value + 1);
+  const subtractValue = () => setValue(value - 1);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='div'>{value}</div>
+      <button className='addButton' onClick={addValue}>+1</button>
+      <button className='subtractButton' onClick={subtractValue}>-1</button>
     </div>
   );
 }
